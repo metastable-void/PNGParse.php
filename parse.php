1fb175d2b794257788584014dbbe139ac146edf5
nord-stream <nord-stream@ochaken.cf>
2016-03-09 14:03:34 +0000

    License and README

--- parse.php
+++ parse.php
@@ -1,5 +1,27 @@
 #!/usr/bin/env php
 <?php
+/**
+	Part of PNGParse.php
+	PNG chunk viewer in PHP, exports complete PNG chunks in JSON!
+
+	Copyright © 2016 All rights reserved.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+	@license GPL-3+
+	@file
+*/
 
 const SIGNATURE = "\x89PNG\r\n\x1a\n";
 
@@ -12,9 +34,26 @@ if (SIGNATURE !== fread($stream, strlen(SIGNATURE))) {
 echo '[', PHP_EOL;
 while (!feof($stream)) {
 	$pos = ftell($stream);
-	$length = end(unpack('N', fread($stream, 4)));
+	$length = fread($stream, 4);
+	if (false === $length) {
+		throw new Exception("I/O error");
+	}
+	
+	$length = end(unpack('N', $length));
 	$type = fread($stream, 4);
-	$data = $length > 0 ? fread($stream, $length) : '';
+	if (false === $type) {
+		throw new Exception("I/O error");
+	}
+	
+	$data = '';
+	while ($length > 0) {
+		$read = fread($stream, $length);
+		if (false === $read) {
+			throw new Exception("I/O error");
+		}
+		$data .= $read;
+		$length -= strlen ($read);
+	}
 	$crc32 = hash('crc32b', $type . $data, true);
 	if (fread($stream, 4) !== $crc32) {
 		throw new Exception("Invalid CRC32 checksum");
@@ -29,8 +68,7 @@ while (!feof($stream)) {
 	
 	if ('IEND' === $type) {
 		echo PHP_EOL;
-		$pos = dechex(ftell($stream));
-		//fwrite(STDERR, "$pos: End of file\n");
+		$pos = ftell($stream);
 		break;
 	}
 	
@@ -38,5 +76,6 @@ while (!feof($stream)) {
 }
 echo ']', PHP_EOL;
 
+fprintf(STDERR, "0x%x: End of file\n", $pos);
 
 // vim: set ts=4 noet ai
